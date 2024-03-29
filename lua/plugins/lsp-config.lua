bae33a476e2dce516a1921fa5464e8600077a889
nidhish-nayak <nidhibelthangady@gmail.com>
2024-03-29 17:30:55 +0530

    Changed harpoon keymaps to Alt

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,14 +31,14 @@ return {
          vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
          vim.keymap.set("n", "<leader>pp", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-         vim.keymap.set("n", "<C-q>", function() harpoon:list():select(1) end)
-         vim.keymap.set("n", "<C-w>", function() harpoon:list():select(2) end)
-         vim.keymap.set("n", "<C-e>", function() harpoon:list():select(3) end)
-         vim.keymap.set("n", "<C-r>", function() harpoon:list():select(4) end)
+         vim.keymap.set("n", "<A-q>", function() harpoon:list():select(1) end)
+         vim.keymap.set("n", "<A-w>", function() harpoon:list():select(2) end)
+         vim.keymap.set("n", "<A-e>", function() harpoon:list():select(3) end)
+         vim.keymap.set("n", "<A-r>", function() harpoon:list():select(4) end)
 
          -- Toggle previous & next buffers stored within Harpoon list
-         vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-         vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+         vim.keymap.set("n", "<A-p>", function() harpoon:list():prev() end)
+         vim.keymap.set("n", "<A-n>", function() harpoon:list():next() end)
       end
    },
    {
