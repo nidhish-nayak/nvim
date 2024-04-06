7a787d5e59ab899098a2a1e611911dfdb8d9f9ea
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-06 16:11:50 +0530

    Minor config changes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,10 +12,18 @@ vim.g.mapleader = " "
 
 -- KEYMAPS --
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- folder tree
-vim.keymap.set({ "n", "v" }, "<C-c>", '"+y')  -- yank motion
-vim.keymap.set({ "n", "v" }, "<C-d>", '"+d')  -- delete motion
-vim.keymap.set("n", "<C-v>", '"+p')           -- paste after cursor
+vim.keymap.set({ "n", "v" }, "<C-c>", '"+y') -- yank motion
+vim.keymap.set({ "n", "v" }, "<C-d>", '"+d') -- delete motion
+vim.keymap.set("n", "<C-v>", '"+p') -- paste after cursor
 
 -- GIT KEYMAPS --
-vim.keymap.set("n", "<leader>grc", ":Git reset --soft HEAD~<CR>", {})                                              -- UNDO GIT COMMIT
-vim.keymap.set("n", "<leader>grs", [[:lua vim.cmd(':Git reset HEAD -- %') vim.cmd(':Git checkout -- %')<CR>]], {}) -- UNSTAGE & RESET FILE
+-- UNDO GIT COMMIT --
+vim.keymap.set("n", "<leader>grc", ":Git reset --soft HEAD~<CR>", {})
+-- UNDO STAGE AND RESET FILE --
+vim.keymap.set("n", "<leader>grs", [[:lua vim.cmd(':Git reset HEAD -- %') vim.cmd(':Git checkout -- %')<CR>]], {})
+
+-- TMUX KEYMAPS --
+vim.keymap.set("n", "<leader>tn", ":terminal tmux new-session %<CR>", {})
+vim.keymap.set("n", "<leader>ta", ":terminal tmux a -t %<CR>", {})
+vim.keymap.set("n", "<leader>td", ":terminal tmux kill-session -t %<CR>", {})
+vim.keymap.set("n", "<leader>tda", ":terminal tmux kill-session -a<CR>", {})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,40 +1,40 @@
 -- LSP SERVERS --
 
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+   {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+         require("mason").setup()
+      end,
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      opts = {
+         auto_install = true,
+      },
+   },
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+         local lspconfig = require("lspconfig")
 
-			-- LIST OF SERVERS --
-			local servers = { "lua_ls", "tsserver", "tailwindcss"}
+         -- LIST OF SERVERS --
+         local servers = { "lua_ls", "tsserver", "tailwindcss" }
 
-			-- RUN SETUP FOR ALL SERVERS ABOVE --
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({
-					capabilities = capabilities,
-				})
-			end
+         -- RUN SETUP FOR ALL SERVERS ABOVE --
+         for _, server in ipairs(servers) do
+            lspconfig[server].setup({
+               capabilities = capabilities,
+            })
+         end
 
-			-- KEYMAPS FOR LSP INSIGHTS --
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+         -- KEYMAPS FOR LSP INSIGHTS --
+         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+         vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      end,
+   },
 }
