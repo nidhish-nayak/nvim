5ec07b918f20fd1af53ae67d4f0f881847a10e6d
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-09 20:03:30 +0530

    Updated final config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,31 +1,31 @@
 -- THEMES --
 
--- return {
---     "catppuccin/nvim",
---     lazy = false,
---     name = "catppuccin",
---     priority = 1000,
---     config = function()
---         require("catppuccin").setup({
---             transparent_background = true,
---         })
---         vim.cmd.colorscheme("catppuccin")
---     end,
--- }
-
 return {
-    "rose-pine/neovim",
+    "catppuccin/nvim",
     lazy = false,
-    name = "rose-pine",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("rose-pine")
-        -- require("rose-pine").setup({
-        --     styles = {
-        --         bold = true,
-        --         italic = true,
-        --         transparency = true,
-        --     },
-        -- })
+        require("catppuccin").setup({
+            transparent_background = true,
+        })
+        vim.cmd.colorscheme("catppuccin")
     end,
 }
+
+-- return {
+--     "rose-pine/neovim",
+--     lazy = false,
+--     name = "rose-pine",
+--     priority = 1000,
+--     config = function()
+--         -- require("rose-pine").setup({
+--         --     styles = {
+--         --         bold = true,
+--         --         italic = true,
+--         --         transparency = true,
+--         --     },
+--         -- })
+--         vim.cmd.colorscheme("rose-pine")
+--     end,
+-- }
