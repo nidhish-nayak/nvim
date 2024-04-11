a3fd65e301cd41f7a9758cf88e6b1d18d170c898
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-11 23:46:59 +0530

    Configured telescope find by reference

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,30 @@
 -- TELESCOPE TO FUZZY SEARCH FILES & GREP --
 
 return {
-   {
-      "nvim-telescope/telescope.nvim",
-      tag = "-1.1.6",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-         local builtin = require("telescope.builtin")
-         vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) -- FIND FILES
-         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- GREP FIND
-         vim.keymap.set("n", "<leader>bb", builtin.buffers, {}) -- FILE TABS (BUFFERS)
-      end,
-   },
-   {
-      "nvim-telescope/telescope-ui-select.nvim",
-      -- This is your opts table
-      config = function()
-         require("telescope").setup({
-            extensions = {
-               ["ui-select"] = {
-                  require("telescope.themes").get_dropdown({}),
-               },
-            },
-         })
-         require("telescope").load_extension("ui-select")
-      end,
-   },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "-1.1.6",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) -- FIND FILES
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- GREP FIND
+            vim.keymap.set("n", "<leader>bb", builtin.buffers, {}) -- FILE TABS (BUFFERS)
+            vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {}) -- FIND REFERENCES
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        -- This is your opts table
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
