62d889fe0bdaafa0a0cd25776a82f8d2b0320636
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-11 22:55:55 +0530

    minor updates

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,12 @@
 -- DETAILS BAR AT THE BOTTOM OF THE TERMINAL --
 
 return {
-   'nvim-lualine/lualine.nvim',
-   config = function()
-      require('lualine').setup({
-         options = {
-            theme = 'dracula'
-         }
-      })
-   end
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "iceberg_dark",
+            },
+        })
+    end,
 }
-
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,31 +1,31 @@
 -- THEMES --
 
-return {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            transparent_background = true,
-        })
-        vim.cmd.colorscheme("catppuccin")
-    end,
-}
-
 -- return {
---     "rose-pine/neovim",
+--     "catppuccin/nvim",
 --     lazy = false,
---     name = "rose-pine",
+--     name = "catppuccin",
 --     priority = 1000,
 --     config = function()
---         -- require("rose-pine").setup({
---         --     styles = {
---         --         bold = true,
---         --         italic = true,
---         --         transparency = true,
---         --     },
---         -- })
---         vim.cmd.colorscheme("rose-pine")
+--         require("catppuccin").setup({
+--             transparent_background = false,
+--         })
+--         vim.cmd.colorscheme("catppuccin")
 --     end,
 -- }
+
+return {
+    "rose-pine/neovim",
+    lazy = false,
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+        -- require("rose-pine").setup({
+        --     styles = {
+        --         bold = true,
+        --         italic = true,
+        --         transparency = true,
+        --     },
+        -- })
+        vim.cmd.colorscheme("rose-pine")
+    end,
+}
