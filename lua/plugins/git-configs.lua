c1689e83ab46aef8122754330496c04af20959a1
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-27 22:57:05 +0530

    Minor changes

--- lua/plugins/git-configs.lua
+++ lua/plugins/git-configs.lua
@@ -1,26 +1,26 @@
 -- MAJOR GIT PLUGINS --
 
 return {
-	{
-		"tpope/vim-fugitive",
-	},
-   {
-      "sindrets/diffview.nvim",
-      config = function ()
-         -- KEYBINDS --
-         vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", {})
-         vim.keymap.set("n", "<leader>gc", ":DiffviewClose<CR>", {})
-         vim.keymap.set("n", "<leader>gf", ":DiffviewToggleFiles<CR>", {})
-      end
-   },
-   {
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "sindrets/diffview.nvim",
+        config = function()
+            -- KEYBINDS --
+            vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", {})
+            vim.keymap.set("n", "<leader>gc", ":DiffviewClose<CR>", {})
+            vim.keymap.set("n", "<leader>gf", ":DiffviewToggleFiles<CR>", {})
+        end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
 
-			-- KEYBINDS --
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-		end,
-	},
+            -- KEYBINDS --
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+        end,
+    },
 }
