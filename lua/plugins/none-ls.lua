49a59ff070c9f44d53a8a0e72ea70352520d65bc
nidhish-nayak <nidhibelthangady@gmail.com>
2024-05-01 00:25:47 +0530

    Added timeout for null-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,23 @@
 -- NONE-LS, ALTERNATIVE OF DEPRICATED NULL-LS --
 
 return {
-   "nvimtools/none-ls.nvim",
-   lazy = false,
-   config = function()
-      local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    lazy = false,
+    config = function()
+        vim.lsp.buf.format({ timeout_ms = 5000 })
+        local null_ls = require("null-ls")
 
-      null_ls.setup({
-         sources = {
-            -- FORMATTERS --
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.prettier,
+        null_ls.setup({
+            sources = {
+                -- FORMATTERS --
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
 
-            -- LINTERS CONFIGURED IN NVIM-LINT --
-         },
-      })
+                -- LINTERS CONFIGURED IN NVIM-LINT --
+            },
+        })
 
-      -- KEYMAPS --
-      vim.keymap.set("n", "<A-f>", vim.lsp.buf.format, {})
-   end,
+        -- KEYMAPS --
+        vim.keymap.set("n", "<A-f>", vim.lsp.buf.format, {})
+    end,
 }
